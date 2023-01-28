2f62f55c16ef9bb3f6cf01f2500e50cde403b0b0
quarioo <kirillzubrilin.zzz@gmail.com>
2023-01-28 22:10:31 +0300

    Start making game logic

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import sys
 
 import pygame
-
 from pygame.locals import *
 
 from start_menu import Button
+from game import Game
 
 FPS = 60
 RUNNING = True
+
 pygame.init()
 
 
-class Game:
+class Main:
     def __init__(self):
         pygame.display.set_caption('Shoot or again?')
 
@@ -21,6 +22,7 @@ class Game:
 
         self.screen = pygame.display.set_mode((1920, 1080))
         self.width, self.height = self.screen.get_width(), self.screen.get_height()
+        print(self.width, self.height)
 
         self.font = pygame.font.Font('./fonts/ZenDots-Regular.ttf', 40)
         start_text = self.font.render('REGISTRATION', True, 'Blue')
@@ -28,9 +30,12 @@ class Game:
         self.objects = []
 
         start_button = Button(
-            self, 'Start', 200, 40, (self.width / 2 - 200 / 2, self.height - 40 / 2), 5
+            self, 'Start', 200, 40, (self.width / 2 - 200 / 2, self.height / 2 - 40 / 2), 5
         )
         self.objects.append(start_button)
+
+        self.is_game_starting = False
+        self.game = Game(self)
 
         while RUNNING:
             self.loop()
@@ -50,8 +55,9 @@ class Game:
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.type == KEYDOWN:
-                pass
+            if self.is_game_starting and event.type == KEYDOWN:
+                self.game.start()
+
 
     def tick(self):
         self.time = self.clock.tick()
@@ -63,4 +69,4 @@ class Game:
 
 
 if __name__ == '__main__':
-    Game()
+    Main()
